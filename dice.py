f7b086874a1b35451be690742489b3e7d545eae2
iamniama <plectrumonthespectrum@gmail.com>
2022-03-08 09:35:20 -0700

    refactored roll method to only return the total, updated text_roll to allow multiple dice types (1d8 2d6) as well as multiple modifiers

--- dice.py
+++ dice.py
@@ -9,7 +9,11 @@ class Dice:
     generate a significant number of rolls (1000) and select random values from among them based
     on the number of dice
     """
-    dice_pool = 1000
+    dice_pool = 50
+    dice_rex = re.compile(r'''
+                (?P<base_roll>(?P<num_dice>[0-9]*)d(?P<sides>[0-9]{,3}))  # get the base roll, dice, and sides
+            ''', re.X)
+    mod_rex = re.compile(r'[+-][0-9]+')
 
     def __init__(self):
         pass
@@ -17,22 +21,29 @@ class Dice:
     @staticmethod
     def roll(dice, sides, modifiers=0):
         """
-        The core of the Dice library, roll generates 1000 random numbers in the given range (sides) and selects
-        a given number (dice) of values to return.
+        The core of the Dice library, roll generates random numbers (scaled based on the number of dice) in the given
+        range (sides) and selects a given number (dice) of values to return.
         :param dice: how many random nums to keep
         :param sides: upper limit of individual randoms
-        :param modifiers: a positive or negative in that affects the value of the roll, defaults to 0
+        :param modifiers: a positive or negative int that affects the value of the roll, defaults to 0
         :return: dict with the number of dice, number of sides, values of each individual die, the value of
         modifier, and an aggregated total of the roll
         """
-        rolls = random.choices([random.randint(1,sides) for x in range(Dice.dice_pool)], k=dice)
-        return {
-            'dice': dice,
-            'sides': sides,
-            'rolls': rolls,
-            'total_modifiers': modifiers,
-            'total': sum(rolls) + modifiers,
-        }
+        return sum(random.choices([random.randint(1,sides) for x in range(Dice.dice_pool * dice)], k=dice)) + modifiers
+
+    @staticmethod
+    def text_roll(cmd_str):
+        """
+        text_roll interprets a natural language dice syntax, such as "4d4 +4" or "1d8
+        :param cmd_str:
+        :return:
+        """
+        dice_info = Dice.dice_rex.search(cmd_str)
+        d_info = Dice.dice_rex.findall(cmd_str)
+        # print([x[0] for x in d_info])
+        modifiers = [int(x) for x in list(Dice.mod_rex.findall(cmd_str))]
+        # return Dice.roll(1 if not dice_info['num_dice'].isdigit() else int(dice_info['num_dice']), int(dice_info['sides']), sum(modifiers))
+        return Dice.multi_roll([a[0] for a in d_info], sum(modifiers))
 
     @staticmethod
     def stat_roll(dice=4, highest=3):
@@ -42,40 +53,30 @@ class Dice:
         :param highest: how many dice to keep, defaults to 3 per 5E rules
         :return: dict with list of individual kept dice values and the total of the kept dice
         """
-        rolls = Dice.roll(dice, 6)['rolls']
-        return {
-            'rolls': list(reversed(sorted(rolls)))[:highest],
-            'total': sum(list(reversed(sorted(rolls)))[:highest])
-        }
+        rolls = [Dice.roll(1, 6) for x in range(dice)]
+        return sum(list(reversed(sorted(rolls)))[:3])
 
     @staticmethod
     def roll_stats():
         """
         Generates a full set of stat rolls for a new character.  In the initial version, this uses one of my
-        old house rules that uses 5 dice instead of the stated 4 from the 5E rules.
+        old house rules that used 5 dice instead of the stated 4 from the 5E rules.
         :return: list of six outputs from stat_roll
         """
-        return [Dice.stat_roll(dice=5)['total'] for x in range(6)]
+        return [Dice.stat_roll(dice=5) for x in range(6)]
 
     @staticmethod
-    def roll_with_advantage(modifiers=0):
+    def roll_with_advantage(modifiers=0, disadvantage=False):
         """
-        Performs an attack or skill check with advantage, wherein two 20 sided dice are rolled, and the higher value
-        is kept
+        Performs an attack or skill check with advantage, wherein two 20 sided dice are rolled and the higher value
+        is kept, or disadvantage, which uses the lower of the two values
         :param modifiers: total value of hit or check modifiers, defaults to 0
-        :return: integer, highest of two rolls plus the value of modifiers
+        :param disadvantage: toggles the method to return a roll with disadvantage instead of advantage
+        :return: integer, highest or lowest of two rolls plus the value of modifiers
         """
-        return max([Dice.roll(1, 20, modifiers)['total'] for x in range(2)])
-
-    @staticmethod
-    def roll_with_disadvantage(modifiers=0):
-        """
-        Performs an attack or skill check with disadvantage, wherein two 20 sided dice are rolled, and the lower value
-        is kept
-        :param modifiers: total value of hit or check modifiers, defaults to 0
-        :return: integer, lowest of two rolls plus the value of modifiers
-        """
-        return min([Dice.roll(1, 20, modifiers)['total'] for x in range(2)])
+        if disadvantage:
+            return min([Dice.roll(1, 20, modifiers) for x in range(2)])
+        return max([Dice.roll(1, 20, modifiers) for x in range(2)])
 
     @staticmethod
     def multi_roll(lst_dice, modifiers=0):
@@ -89,12 +90,12 @@ class Dice:
         total = 0
         for x in lst_dice:
             dice, sides = [int(y) for y in x.split('d')]
-            total += Dice.roll(dice, sides)['total']
+            total += Dice.roll(dice, sides)
         return total + modifiers
 
 
 if __name__ == "__main__":
-    print("\n****************Base Roll***************************")
+    print("\n****************Base Roll (6d6 + 10)***************************")
     print(Dice.roll(6,6,10))
     print("\n****************Stat Roll***************************")
     print(Dice.stat_roll())
@@ -102,5 +103,13 @@ if __name__ == "__main__":
     print(*Dice.roll_stats(), sep="\n")
     print("\n****************Advantage Roll***************************")
     print(Dice.roll_with_advantage(-2))
-    print("\n****************Multi Roll***************************")
+    print("\n****************Multi Roll d8 and 2d6***************************")
     print(Dice.multi_roll(['1d8', '2d6'], 5))
+    print("\n****************Natural Language Roll***************************")
+    print("Command: '1d8 2d6 +2 +1 -1'")
+    print(Dice.text_roll("1d8 2d6 +2 +1 -1"))
+    for _ in range(4):
+        print("***********************************************")
+        print(*Dice.roll_stats(), sep="\n")
+
+    print("***********************************************")
