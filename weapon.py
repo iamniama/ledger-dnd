c6f50514df4ec3aa2bfde41c76b63a93dd329d0d
iamniama <plectrumonthespectrum@gmail.com>
2022-03-09 17:03:41 -0700

    added to_json to Weapon

--- weapon.py
+++ weapon.py
@@ -1,3 +1,5 @@
+import json
+
 class Weapon:
     def __init__(self, name, damage_type, damage, bonuses=0):
         self.name = name
@@ -11,6 +13,17 @@ class Weapon:
             f'Name: {self.name}, {self.damage}{"+"+str(self.bonuses) if self.bonuses != 0 else ""} {self.damage_type}'
         )
 
+    def to_json(self):
+        return json.dumps(
+            {
+                'NAME': self.name,
+                'DAMAGE': self.damage,
+                'DAMAGE_TYPE': self.damage_type,
+                'BONUSES': self.bonuses,
+                'DESCRIPTION': self.description
+            }
+        )
+
     def set_description(self, desc):
         self.description = desc
 
@@ -18,5 +31,7 @@ class Weapon:
 if __name__ == "__main__":
     lsword = Weapon('longsword', 'slashing', '1d8')
     p1lsword = Weapon('longsword', 'slashing', '1d8', 1)
+    p1lsword.set_description("A finely made sword bearing faint, silvery runes of enchantment")
     print(lsword)
     print(p1lsword)
+    print(p1lsword.to_json())
